b8b281b9708ccbd5bb01405b806f8125974265c4
Demo User <debian@beaglebone.localdomain>
2019-08-27 13:37:53 +0000

    backup ledclicked

--- unidade1/ledclicked/ledclicked.cpp
+++ unidade1/ledclicked/ledclicked.cpp
@@ -6,81 +6,68 @@
 #include <unistd.h>
 
 using namespace BlackLib;
-using std::cout;
+using namespace std;
+
+#define TIME 25
 
 /* ======= DEFINES  ===== */
 
 BlackLib::gpioName leds_gpios[] =    {GPIO_44,GPIO_67,GPIO_46,GPIO_61,GPIO_66,GPIO_45};
 BlackLib::gpioName buttons_gpios[] = {GPIO_68,GPIO_26,GPIO_65,GPIO_69,GPIO_20,GPIO_60};
 
-//#define TIME 30.0
-#define N 6
+static BlackGPIO led[] = 	{ BlackGPIO(leds_gpios[0], output),
+						  			  BlackGPIO(leds_gpios[1], output),
+						  			  BlackGPIO(leds_gpios[2], output),
+						  	 	      BlackGPIO(leds_gpios[3], output),
+						  			  BlackGPIO(leds_gpios[4], output),
+						  	 	      BlackGPIO(leds_gpios[5], output)};
+
+static BlackGPIO button[] = { BlackGPIO(buttons_gpios[0], input),
+						  			   BlackGPIO(buttons_gpios[1], input),
+						  	  	 	   BlackGPIO(buttons_gpios[2], input),
+						  		       BlackGPIO(buttons_gpios[3], input),
+						  			   BlackGPIO(buttons_gpios[4], input),
+						  			   BlackGPIO(buttons_gpios[5], input)};
+
 
 double timer(void);
+void menu();
+void game(double t_hold, time_t t_wait, time_t t_game);
 
 int main(int argc, char **argv){
-  unsigned int hits = 0;
-  unsigned int miss = 0;
-  unsigned int mole;
-  time_t t_game;
-  time_t start;
-  time_t sec = 0;
-  time_t t_wait; 
-  double ledsec_start = 0;
-  double t_hold;
-  
-  if(argc != 4){
-	  perror("PASSE US ARGUMETU\n");
-	  exit(1);
- }
-  t_hold = atof(argv[1]);
-  t_wait = atoi(argv[2]);
-  t_game = atoi(argv[3]);
+  unsigned int k;  
 
 
-  BlackGPIO led[] = 	{ BlackGPIO(leds_gpios[0], output),
-						  BlackGPIO(leds_gpios[1], output),
-						  BlackGPIO(leds_gpios[2], output),
-						  BlackGPIO(leds_gpios[3], output),
-						  BlackGPIO(leds_gpios[4], output),
-						  BlackGPIO(leds_gpios[5], output)};
+  while(1){
+    menu();
+	cin >> k;
+    
+	switch(k){
+    case 0: //facil
+		game(2.0, 500, TIME);
+    break;
+	case 1: //medio
+		game(1.0, 50, TIME);
+    break;
+	case 2: //dificil
+		game(0.5, 50, TIME);
+    break; 
+	case 3: //insano
+		game(0.3, 50, TIME);
+    break;
+	case 4: //aventura
+		game(0.3, 50, TIME);
+    break;
+	case 5: //sair
+		cout << "Vlw, flws!\n";
+		exit(0);
+	break;
+	default:
+	break;
+	}
 
-  BlackGPIO button[] = { BlackGPIO(buttons_gpios[0], input),
-						  BlackGPIO(buttons_gpios[1], input),
-						  BlackGPIO(buttons_gpios[2], input),
-						  BlackGPIO(buttons_gpios[3], input),
-						  BlackGPIO(buttons_gpios[4], input),
-						  BlackGPIO(buttons_gpios[5], input)};
+  }
 
-  cout << "ONLINE!\n";
-  start = timer();
-
-  srand(time(NULL));
-  while(sec <= t_game){
-    miss ++;
-    mole = rand()%N;
-    cout << "MOLE:" << mole << std::endl;
-    led[mole].setValue(high);
-
-    ledsec_start = timer();
-    while(timer() - ledsec_start < t_hold){
-      if(button[mole].getNumericValue() == 1){
-        hits++;
-        cout << "HIT!!\n";
-        break;
-      }
-    }
-    led[mole].setValue(low);
-    usleep(t_wait * 1000); 
-    //usleep(t_wait); 
-
-
-    sec = timer() - start;
-    printf("Tempo:%d segundos Hits:%d\n", sec, hits);
-  };
-
-  cout << "ACABOU SEU TEMPO!\n";
-  printf("Hits:%d, MISS:%d\n", hits, miss-hits);
 
   return 0;
 }
@@ -98,4 +85,50 @@ double timer(void)
     tbase = t;
   }
   return( t.tv_sec-tbase.tv_sec + (t.tv_usec-tbase.tv_usec)/1000000.0 );
+}
+
+void game(double t_hold, time_t t_wait, time_t t_game)
+{
+  #define N 6
+
+  unsigned int hits = 0;
+  unsigned int miss = 0;
+  unsigned int mole;
+  time_t start;
+  time_t sec = 0;
+  double ledsec_start = 0;
+  
+  start = timer();
+
+  srand(time(NULL));
+  while(sec <= t_game){
+    miss ++;
+    mole = rand()%N;
+    led[mole].setValue(high);
+
+    ledsec_start = timer();
+    while(timer() - ledsec_start < t_hold){
+      if(button[mole].getNumericValue() == 1){
+        hits++;
+        break;
+      }
+    }
+    led[mole].setValue(low);
+    usleep(t_wait * 1000);  
+
+    sec = timer() - start;
+  };
+  printf("Hits:%d, MISS:%d\n", hits, miss-hits);
+}
+
+void menu(){
+	cout << "LEDCLICKED ON\n";
+
+    cout << "Escolha!:\n";
+	cout << "0 - Modo Facil\n";
+	cout << "1 - Modo Medio\n";
+	cout << "2 - Modo Dificil\n";
+	cout << "3 - Modo Insano\n";
+	cout << "4 - Modo Aventura\n";
+	cout << "5 - Sair\n";
 }
